7ba76f9db6aea684167370f436f2ec4a253e4335
Iman Sharifi <imansharifi244@gmail.com>
2022-09-23 20:37:28 +0330

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors({
-    origin: '/.pro3dsky.com$/',
+    origin: '/http://localhost:3000/',
     credentials: true,
   });
   app.use(cookieParser());
